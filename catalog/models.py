285816f2ef17be7229c94b2f9789abc889aa5427
AndreyGTHB <andr.g.tuk@gmail.com>
2020-09-07 17:01:24 +0700

    Adding models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, help_text="Enter a book genre here")
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, help_text="Enter a book name here")

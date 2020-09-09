721b18f6146156d544c9d007028de3a04ea9dd53
AndreyGTHB <andr.g.tuk@gmail.com>
2020-09-09 18:19:40 +0700

    Working at admin models

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,12 +4,28 @@ from .models import *
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = [('first_name', 'last_name'), ('date_of_birth', 'date_of_death')]
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0  # So that the fields for additional instances are not created automatically
 
 class BookAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline]
 
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('book', 'status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 admin.site.register(Book, BookAdmin)

2997427bed32d4ea37077747133df9ae2dc817d1
AndreyGTHB <andr.g.tuk@gmail.com>
2020-09-13 17:08:38 +0700

    Some changes

--- catalog/views.py
+++ catalog/views.py
@@ -3,10 +3,10 @@ from .models import *
 
 
 def index(request):
-    num_books = Book.objects.all().count()
+    num_books = Book.objects.count()
     num_instances = BookInstance.objects.all().count()
 
-    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_instances_available = BookInstance.objects.all().filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
     response = render(
@@ -15,3 +15,7 @@ def index(request):
         context={'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors}
     )
     return response
+
+
+def book_list(request):
+    num_books = Book.objects.count()

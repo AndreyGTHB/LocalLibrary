5756591f3b9d3a2d94c85e527a3170b51a674bcf
AndreyGTHB <andr.g.tuk@gmail.com>
2020-09-17 14:09:47 +0700

    Some changes

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import django
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = []
-urlpatterns += path('admin/', admin.site.urls),
-urlpatterns += path('catalog/', include('catalog.urls')),
-urlpatterns += path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    # path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+    # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += [path('admin/', admin.site.urls)]
+urlpatterns += [path('catalog/', include('catalog.urls'))]
+urlpatterns += [path('', RedirectView.as_view(url='/catalog/', permanent=True))]
+urlpatterns += [path('accounts/', include('django.contrib.auth.urls'))]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
